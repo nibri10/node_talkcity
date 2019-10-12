9eacd785549eaa9016303c6312db03e5d72e2bbd
Nicolas dos Reis Barbosa de Oliveira <nibri.kcond2011@gmail.com>
2019-10-12 13:10:20 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', (req, res, next) => {
 });
 
 const problem = require('../Controller/ProblemController');
+const user = require('../Controller/UserController');
 
 // Create a new Note
 router.post('/problem', problem.create);
@@ -24,6 +25,22 @@ router.put('/problem/:problemId', problem.update);
 
 // Delete a Note with noteId
 router.delete('/problem/:problemId', problem.delete);
+
+
+// Create a new Note
+router.post('/user', user.create);
+
+// Retrieve all Notes
+router.get('/user', user.findAll);
+
+// Retrieve a single Note with noteId
+router.get('/user/:userId', user.findOne);
+
+// Update a Note with noteId
+router.put('/user/:userId', user.update);
+
+// Delete a Note with noteId
+router.delete('/user/:user', user.delete);
 
 
 module.exports = router;
