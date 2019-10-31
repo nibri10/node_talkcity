93e4693cbcb2dfa3b72fc6ed21a3581554f3ac59
Nicolas dos Reis Barbosa de Oliveira <nibri.kcond2011@gmail.com>
2019-10-30 21:34:29 -0300

    Update MacAdress.js

--- model/MacAdress.js
+++ model/MacAdress.js
@@ -1,27 +1,17 @@
 const mongoose = require('mongoose');
 
 const MacSchema = mongoose.Schema({
-    idproblem:{
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "Problem"
+
+      idproblem:{
+        type:String,
+        require:true
     },
 
     macAdress:{
-       type: mongoose.Schema.Types.ObjectId, 
-       ref: "User"
-
-    },
-  
-    like_problem:{
         type:String,
         require:true
 
-    },
-    dontlike_problem:{
-        type:String,
-        require:true
     }
-
 });
 
 module.exports = mongoose.model('Mac', MacSchema);
